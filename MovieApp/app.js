f84f5cee1b01a5b1a5f4c099e3249d32447afeee
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-10-08 20:52:21 +0300

    Movie App with API

---
+++ MovieApp/app.js
@@ -0,0 +1,77 @@
+const API_URL =
+    'https:api.themoviedb.org/4/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API =
+    'https://api.themoviedb.org/4/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+    const results = await fetch(url);
+    const data = await results.json(); // gelen sonucu json formatina donustur 
+
+    console.log(data.results);
+    showMovies(data.results);
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()  // sayfa yeniden yuklenmesin
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== "") {
+        getMovies(SEARCH_API + searchTerm);
+
+        search.value = "";
+    } else {
+        window.location.reload();  // aranan kelime yoksa sayfayi yeniden yuklesin
+    }
+})
+
+function showMovies(movies) {
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { title, poster_path, overview, vote_average } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie"); // movie classli bir div olusturduk
+
+        movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}"
+        alt="${title}" />
+
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>${title} <small>overview</small> </h3>
+            <p>
+                ${overview} 
+            </p>
+        </div>
+        `;
+
+        main.appendChild(movieEl);  // main kisminin altina (appendChild) ekle
+    });
+}
+
+function getClassByRate(vote){   // film oyuna/puanina gore puan rengini degistirme fonksiyonu
+    if(vote >= 8){
+        return "green";
+    } else if(vote >= 5){
+        return "orange";
+    } else{
+        return "red";
+    }
+}
+
+
+
+
